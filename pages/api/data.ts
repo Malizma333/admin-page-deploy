b9a9619cd5bcbb0d521320fc5020d52cb62d9a9a
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-12-05 21:08:50 -0600

    changed data retrieval to mymlh

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,20 +1,34 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from '../../lib/mongodb';
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-	if (req.method === 'GET') {
-		try {
-			const client = await clientPromise;
-			const db = client.db('main');
+    if (req.method === "GET") {
+        try {
+            const accessToken = req.headers.authorization?.split(" ")[1];
 
-			const data = await db.collection('applications').find().toArray();
+            if (!accessToken) {
+                res.status(401).json({ error: "No authorization token provided" });
+                return;
+            }
 
-			res.status(200).json(data);
-		} catch (e) {
-			console.error(e);
-			res.status(500).send('An error occured');
-		}
-	} else {
-		res.status(405).send('Must use GET');
-	}
+            const response = await axios.get("https://api.mlh.com/v4/users/me?expand[]=education&expand[]=professional_experience&expand[]=address", {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+
+            res.status(200).json(response.data);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                res.status(error.response?.status || 500).json({
+                    error: error.response?.data || "An error occurred while fetching MLH data",
+                });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: "Internal server error" });
+            }
+        }
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
+    }
 };

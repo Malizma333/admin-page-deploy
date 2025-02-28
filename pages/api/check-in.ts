6bd23303324e288eec5a59ed31127231d0423597
Jason Xu <Xinwen31415@gmail.com>
2025-02-27 22:21:55 -0600

    added type coersion to check in page

--- pages/api/check-in.ts
+++ pages/api/check-in.ts
@@ -20,7 +20,32 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
 				.updateOne({ first_name: req.body.firstName, last_name: req.body.lastName }, { $set: { checkedIn: !application.checkedIn } });
 
 			application.checkedIn = !application.checkedIn;
-			res.status(200).send(application);
+			res.status(200).send({
+				_id: application._id,
+				firstName: application.first_name,
+				lastName: application.last_name,
+				email: application.email,
+				age: application.profile?.age?.toString() || '',
+				phoneNumber: application.phone_number || '',
+				country: application.profile?.country_of_residence || '',
+				school: application.education?.[0]?.school_name || '',
+				levelOfStudy: application.education?.[0]?.school_type || '',
+				graduationMonth: application.education?.[0]?.end_date
+					? new Date(application.education[0].end_date * 1000).toLocaleString('default', { month: 'long' })
+					: '',
+				graduationYear: application.education?.[0]?.end_date ? new Date(application.education[0].end_date * 1000).getFullYear().toString() : '',
+				shirtSize: (application.profile?.tshirt_size as Application['shirtSize']) || 'M',
+				dietRestrictions: application.profile?.dietary_preference ? [application.profile.dietary_preference] : [],
+				resume: null,
+				codeOfConductAgreement: true,
+				dataAgreement: true,
+				mlhAgreement: true,
+				category: null,
+				featured: false,
+				projectLink: null,
+				projectName: null,
+				checkedIn: application.checkedIn
+			});
 		} catch (e) {
 			console.error(e);
 			res.status(500).send('An error occured');
